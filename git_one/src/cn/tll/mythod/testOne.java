ce35a4d5d9af1f61700d19ba7c85e14400110fd8
Tll516330 <2289091393@qq.com>
2020-03-08 15:51:51 +0800

    初始化项目项目1

--- git_one/src/cn/tll/mythod/testOne.java
+++ git_one/src/cn/tll/mythod/testOne.java
@@ -3,5 +3,6 @@ package cn.tll.mythod;
 public class testOne {
     public static void main(String[] args) {
         System.out.println("这是一个熟悉git的方法");
+        System.out.println("dffdfdfd");
     }
 }
